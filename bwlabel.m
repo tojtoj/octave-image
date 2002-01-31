5b9e5af2d22e223c347ba4ff5e682f0d51b89678
alnd <alnd@localhost>
2002-01-31 06:11:53 +0000

    Cosmetic changes in the comment section for 'Copyright' and/or 'Author' lines to enable parsing by the admin/get_authors script and automatic generation of the AUTHORS file.  No changes to executable code!

--- bwlabel.m
+++ bwlabel.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2000  Etienne Grossman
+## Copyright (C) 2000  Etienne Grossmann
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
