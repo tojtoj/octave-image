ae8c7d1dd84a5f7adb083407b2e67e66bd8c0044
pkienzle <pkienzle@localhost>
2005-05-25 13:42:56 +0000

    More author/copyright consistency changes

--- imrotate.m
+++ imrotate.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2004-2005 Justus Piater
+## Copyright (C) 2004-2005 Justus H. Piater
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License
