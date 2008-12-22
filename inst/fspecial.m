48c9338cdfc9ed014648e87f2ab710af19355f9c
hauberg <hauberg@localhost>
2008-12-22 14:18:26 +0000

    Improve help message

--- inst/fspecial.m
+++ inst/fspecial.m
@@ -18,44 +18,75 @@
 ## Create spatial filters for image processing.
 ##
 ## @var{type} determines the shape of the filter and can be
-## @table @code
-## @item 'average'
-## Rectangular averaging filter.
-## @item 'disk'
-## Circular averaging filter.
-## @item 'gaussian'
-## Gaussian filter.
-## @item 'log'
-## Laplacian of Gaussian.
-## @item 'laplacian'
-## 3x3 approximation of the laplacian.
-## @item 'unsharp'
-## Sharpening filter.
-## @item 'motion'
-## Moion blur filter of width 1 pixel.
-## @item 'sobel'
-## Horizontal Sobel edge filter.
-## @item 'prewitt'
-## Horizontal Prewitt edge filter.
-## @end table
-##
-## The parameters that need to be specified depend on the filtertype.
-##
-## Examples of use and associated default values:
+## @table @t
+## @item "average"
+## Rectangular averaging filter. The optional argument @var{arg1} controls the
+## size of the filter. If @var{arg1} is an integer @var{N}, a @var{N} by @var{N}
+## filter is created. If it is a two-vector with elements @var{N} and @var{M}, the
+## resulting filter will be @var{N} by @var{M}. By default a 3 by 3 filter is
+## created.
+## @item "disk"
+## Circular averaging filter. The optional argument @var{arg1} controls the
+## radius of the filter. If @var{arg1} is an integer @var{N}, a 2 @var{N} + 1
+## filter is created. By default a radius of 5 is used.
+## @item "gaussian"
+## Gaussian filter. The optional argument @var{arg1} controls the size of the
+## filter. If @var{arg1} is an integer @var{N}, a @var{N} by @var{N}
+## filter is created. If it is a two-vector with elements @var{N} and @var{M}, the
+## resulting filter will be @var{N} by @var{M}. By default a 3 by 3 filter is
+## created. The optional argument @var{arg2} sets spread of the filter. By default
+## a spread of @math{0.5} is used.
+## @item "log"
+## Laplacian of Gaussian. The optional argument @var{arg1} controls the size of the
+## filter. If @var{arg1} is an integer @var{N}, a @var{N} by @var{N}
+## filter is created. If it is a two-vector with elements @var{N} and @var{M}, the
+## resulting filter will be @var{N} by @var{M}. By default a 5 by 5 filter is
+## created. The optional argument @var{arg2} sets spread of the filter. By default
+## a spread of @math{0.5} is used.
+## @item "laplacian"
+## 3x3 approximation of the laplacian. The filter is approximated as
 ## @example
-##  f = fspecial('average',sze)           # sze can be a 1 or 2 vector
-##                                        # default is [3, 3].
-##  f = fspecial('disk',radius)           # default radius = 5
-##  f = fspecial('gaussian',sze, sigma)   # default sigma is 0.5
-##  f = fspecial('laplacian',sze, sigma)  # default sze is [5, 5]
-##                                        # default sigma is 0.5
-##  f = fspecial('log');
-##  f = fspecial('motion', length, angle) # default length is 9
-##                                        # default angle is 0 (degrees)
-##  f = fspecial('sobel');
-##  f = fspecial('prewitt');
+## (4/(@var{alpha}+1))*[@var{alpha}/4,     (1-@var{alpha})/4, @var{alpha}/4; ...
+##                (1-@var{alpha})/4, -1,          (1-@var{alpha})/4;  ...
+##                @var{alpha}/4,     (1-@var{alpha})/4, @var{alpha}/4];
 ## @end example
-## When sze is specified as a single value the filter will be square.
+## where @var{alpha} is a number between 0 and 1. This number can be controlled
+## via the optional input argument @var{arg1}. By default it is @math{0.2}.
+## @item "unsharp"
+## Sharpening filter. The following filter is returned
+## @example
+## (1/(@var{alpha}+1))*[-@var{alpha},   @var{alpha}-1, -@var{alpha}; ...
+##                 @var{alpha}-1, @var{alpha}+5,  @var{alpha}-1; ...
+##                -@var{alpha},   @var{alpha}-1, -@var{alpha}];
+## @end example
+## where @var{alpha} is a number between 0 and 1. This number can be controlled
+## via the optional input argument @var{arg1}. By default it is @math{0.2}.
+## @item "motion"
+## Moion blur filter of width 1 pixel. The optional input argument @var{arg1}
+## controls the length of the filter, which by default is 9. The argument @var{arg2}
+## controls the angle of the filter, which by default is 0 degrees.
+## @item "sobel"
+## Horizontal Sobel edge filter. The following filter is returned
+## @example
+## [ 1,  2,  1;
+##   0,  0,  0;
+##  -1, -2, -1 ]
+## @end example
+## @item "prewitt"
+## Horizontal Prewitt edge filter. The following filter is returned
+## @example
+## [ 1,  1,  1;
+##   0,  0,  0;
+##  -1, -1, -1 ]
+## @end example
+## @item "kirsch"
+## Horizontal Kirsch edge filter. The following filter is returned
+## @example
+## [ 3,  3,  3;
+##   3,  0,  3;
+##  -5, -5, -5 ]
+## @end example
+## @end table
 ## @end deftypefn
 
 ## Remarks by Søren Hauberg (jan. 2nd 2007)
@@ -63,99 +94,99 @@
 ## GPL'ed implementation of fspecial from 
 ## http://www.csse.uwa.edu.au/~pk/research/matlabfns/OctaveCode/fspecial.m
 
-function [ f ] = fspecial (type, arg1, arg2)
-  if (!ischar(type))
-    error("fspecial: first argument must be a string");
+function f = fspecial (type, arg1, arg2)
+  if (!ischar (type))
+    error ("fspecial: first argument must be a string");
   endif
   
   switch lower(type)
     case "average"
       ## Get filtersize
-      if (nargin > 1 && isreal(arg1) && length(arg1(:)) <= 2)
-        fsize = arg1(:);
+      if (nargin > 1 && isreal (arg1) && length (arg1 (:)) <= 2)
+        fsize = arg1 (:);
       else
         fsize = 3;
       endif
       ## Create the filter
-      f = ones(fsize);
+      f = ones (fsize);
       ## Normalize the filter to integral 1
-      f = f/sum(f(:));
+      f = f / sum (f (:));
 
     case "disk"
       ## Get the radius
-      if (nargin > 1 && isreal(arg1) && length(arg1(:)) == 1)
+      if (nargin > 1 && isreal (arg1) && length (arg1 (:)) == 1)
         radius = arg1;
       else
         radius = 5;
       endif
       ## Create the filter
-      [x,y] = meshgrid(-radius:radius, -radius:radius);
-      r = sqrt(x.^2 + y.^2);
+      [x, y] = meshgrid (-radius:radius, -radius:radius);
+      r = sqrt (x.^2 + y.^2);
       f = (r <= radius);
       ## Normalize the filter to integral 1
-      f = f/sum(f(:));
+      f = f / sum (f (:));
 
     case "gaussian"
       ## Get hsize
-      if (nargin > 1 && isreal(arg1))
-        if (length(arg1(:)) == 1)
+      if (nargin > 1 && isreal (arg1))
+        if (length (arg1 (:)) == 1)
           hsize = [arg1, arg1];
-        elseif (length(arg1(:)) == 2)
+        elseif (length (arg1 (:)) == 2)
           hsize = arg1;
         else
-          error("fspecial: second argument must be a scalar or a vector of two scalars");
+          error ("fspecial: second argument must be a scalar or a vector of two scalars");
         endif
       else
         hsize = [3, 3];
       endif
       ## Get sigma
-      if (nargin > 2 && isreal(arg2) && length(arg2(:)) == 1)
+      if (nargin > 2 && isreal (arg2) && length (arg2 (:)) == 1)
         sigma = arg2;
       else
         sigma = 0.5;
       endif
-      h1 = hsize(1)-1; h2 = hsize(2)-1; 
+      h1 = hsize (1)-1; h2 = hsize (2)-1; 
       [x, y] = meshgrid(0:h2, 0:h1);
       x = x-h2/2; y = y-h1/2;
       gauss = exp( -( x.^2 + y.^2 ) / (2*sigma^2) );
-      f = gauss/sum(gauss(:));
+      f = gauss / sum (gauss (:));
 
     case "laplacian"
       ## Get alpha
-      if (nargin > 1 && isscalar(arg1))
+      if (nargin > 1 && isscalar (arg1))
         alpha = arg1;
-        if (alpha<0 | alpha>1)
-          error("fspecial: second argument must be between 0 and 1");
+        if (alpha < 0 || alpha > 1)
+          error ("fspecial: second argument must be between 0 and 1");
         endif
       else
         alpha = 0.2;
       endif
       ## Compute filter
-      f = (4/(alpha+1))*[alpha/4, (1-alpha)/4, alpha/4; ...
-                         (1-alpha)/4, -1, (1-alpha)/4;  ...
-                         alpha/4, (1-alpha)/4, alpha/4];
+      f = (4/(alpha+1))*[alpha/4,     (1-alpha)/4, alpha/4; ...
+                         (1-alpha)/4, -1,          (1-alpha)/4;  ...
+                         alpha/4,     (1-alpha)/4, alpha/4];
     case "log"
       ## Get hsize
-      if (nargin > 1 && isreal(arg1))
-        if (length(arg1(:)) == 1)
+      if (nargin > 1 && isreal (arg1))
+        if (length (arg1 (:)) == 1)
           hsize = [arg1, arg1];
-        elseif (length(arg1(:)) == 2)
+        elseif (length (arg1 (:)) == 2)
           hsize = arg1;
         else
-          error("fspecial: second argument must be a scalar or a vector of two scalars");
+          error ("fspecial: second argument must be a scalar or a vector of two scalars");
         endif
       else
         hsize = [5, 5];
       endif
       ## Get sigma
-      if (nargin > 2 && isreal(arg2) && length(arg2(:)) == 1)
+      if (nargin > 2 && isreal (arg2) && length (arg2 (:)) == 1)
         sigma = arg2;
       else
         sigma = 0.5;
       endif
       ## Compute the filter
-      h1 = hsize(1)-1; h2 = hsize(2)-1; 
-      [x y] = meshgrid(0:h2, 0:h1);
+      h1 = hsize (1)-1; h2 = hsize (2)-1; 
+      [x, y] = meshgrid(0:h2, 0:h1);
       x = x-h2/2; y = y = y-h1/2;
       gauss = exp( -( x.^2 + y.^2 ) / (2*sigma^2) );
       f = ( (x.^2 + y.^2 - 2*sigma^2).*gauss )/( 2*pi*sigma^6*sum(gauss(:)) );
@@ -164,58 +195,58 @@ function [ f ] = fspecial (type, arg1, arg2)
       ## Taken (with some changes) from Peter Kovesis implementation 
       ## (http://www.csse.uwa.edu.au/~pk/research/matlabfns/OctaveCode/fspecial.m)
       ## FIXME: The implementation is not quite matlab compatible.
-      if (nargin > 1 && isreal(arg1))
+      if (nargin > 1 && isreal (arg1))
         len = arg1;
       else
         len = 9;
       endif
-      if (mod(len,2) == 1)
+      if (mod (len, 2) == 1)
         sze = [len, len];
       else
         sze = [len+1, len+1];
       end
-      if (nargin > 2 && isreal(arg2))
+      if (nargin > 2 && isreal (arg2))
         angle = arg2;
       else
         angle = 0;
       endif
 	    
       ## First generate a horizontal line across the middle
-      f = zeros(sze);
-      f(floor(len/2)+1,1:len) = 1;   
+      f = zeros (sze);
+      f (floor (len/2)+1, 1:len) = 1;
 
       # Then rotate to specified angle
-      f = imrotate(f, angle, 'bilinear', 'loose');
-      f = f/sum(f(:));
+      f = imrotate (f, angle, "bilinear", "loose");
+      f = f / sum (f (:));
 
     case "prewitt"
       ## The filter
-      f = [1 1 1; 0 0 0; -1 -1 -1];
+      f = [1, 1, 1; 0, 0, 0; -1, -1, -1];
       
     case "sobel"
       ## The filter
-      f = [1 2 1; 0 0 0; -1 -2 -1];
+      f = [1, 2, 1; 0, 0, 0; -1, -2, -1];
       
     case "kirsch"
       ## The filter
-      f = [3 3 3; 3 0 3; -5 -5 -5];
+      f = [3, 3, 3; 3, 0, 3; -5, -5, -5];
     
     case "unsharp"
       ## Get alpha
-      if (nargin > 1 && isscalar(arg1))
+      if (nargin > 1 && isscalar (arg1))
         alpha = arg1;
-        if (alpha<0 | alpha>1)
-          error("fspecial: second argument must be between 0 and 1");
+        if (alpha < 0 || alpha > 1)
+          error ("fspecial: second argument must be between 0 and 1");
         endif
       else
         alpha = 0.2;
       endif
       ## Compute filter
-      f = (1/(alpha+1))*[-alpha,  alpha-1, -alpha; ...
-                         alpha-1, alpha+5, alpha-1; ...
-                         -alpha,  alpha-1, -alpha];
+      f = (1/(alpha+1))*[-alpha,   alpha-1, -alpha; ...
+                          alpha-1, alpha+5,  alpha-1; ...
+                         -alpha,   alpha-1, -alpha];
 
     otherwise
-      error("fspecial: filter type '%s' is not supported", type);
+      error ("fspecial: filter type '%s' is not supported", type);
   endswitch
 endfunction
