11f0cc3bdaa620e2539a9a1995b6d36589684f8a
hauberg <hauberg@localhost>
2009-04-03 19:51:16 +0000

    remove overshooting handling

--- inst/imremap.m
+++ inst/imremap.m
@@ -91,11 +91,7 @@ function [warped, valid] = imremap(im, XI, YI, interp = "bilinear", extrapval = 
 
   ## Change the class of the results according to the class of the image
   c = class(im);
-  if (strcmpi(c, "double"))
-    ## Remove overshooting caused by bicubic interpolation
-    warped(warped>1) = 1;
-    warped(warped<0) = 0;
-  elseif (strcmpi(c, "uint8"))
+  if (strcmpi(c, "uint8"))
     warped = uint8(warped);
   elseif (strcmpi(c, "uint16"))
     warped = uint16(warped);
