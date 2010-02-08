4e00b39cc0a655d86ad52e28df23b6f5ae5c62d9
hauberg <hauberg@localhost>
2010-02-08 01:25:31 +0000

    Document bwboundaries

--- inst/bwboundaries.m
+++ inst/bwboundaries.m
@@ -13,6 +13,38 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{boundaries} = } bwboundaries (@var{BW})
+## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries (@var{BW}, @
+## @var{connectivity})
+## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries (@var{BW}, @
+## @var{connectivity}, @var{options})
+## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}] = } bwboundaries (@dots{})
+## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}, @var{num_labels}] @
+## = } bwboundaries (@dots{})
+## Trace the boundaries of the objects in a binary image.
+##
+## @var{boundaries} is a cell array in which each element is the boundary of an
+## object in the binary image @var{BW}. The boundary of an object is represented
+## by a @var{K} by 2 matrix where each row contains the @var{(x, y)} coordinates
+## of a point on the boundary.
+##
+## By default the boundaries are computed using 4-connectivity. This can be 
+## changed to 8-connectivity by setting @var{connectivity} to 8. Sadly, this
+## feature is not yet implemented.
+##
+## By default @code{bwboundaries} computes all boundaries in the image, i.e.
+## both interior and exterior object boundaries. This behaviour can be changed
+## through the @var{options} input argument. If this is the string @t{"holes"}
+## both boundary types are considered. If it is instead @t{"noholes"}, only exterior
+## boundaries will be traced.
+##
+## If two or more output arguments are requested, the algorithm also computes
+## the labelled image as returned by @code{bwlabel} in @var{labels}. The number
+## of labels in this image is optionally returned in @var{num_labels}.
+## @seealso{bwlabel}
+## @end deftypefn
+
 function [B, L, num_labels] = bwboundaries (bw, N = 4, options = "holes")
   ## Check input
   if (nargin < 1)
