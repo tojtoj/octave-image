89db731a79440c08de1af3a835a246c8dbf7d673
hauberg <hauberg@localhost>
2010-12-22 23:02:14 +0000

    New version of bwboundaries (from A Kelly)

--- inst/bwboundaries.m
+++ inst/bwboundaries.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2010 Soren Hauberg
+## Modified December 2010 by Andrew Kelly, IPS Radio & Space Services
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -14,87 +15,81 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{boundaries} = } bwboundaries (@var{BW})
-## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries (@var{BW}, @
-## @var{connectivity})
-## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries (@var{BW}, @
-## @var{connectivity}, @var{options})
-## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}] = } bwboundaries (@dots{})
-## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}, @var{num_labels}] @
-## = } bwboundaries (@dots{})
+## @deftypefn {Function File} {@var{boundaries} = } bwboundaries(@var{BW})
+## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries(@var{BW}, @var{conn})
+## @deftypefnx {Function File} {@var{boundaries} = } bwboundaries(@var{BW}, @var{conn}, @var{holes})
+## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}] = } bwboundaries(@dots{})
+## @deftypefnx {Function File} {[@var{boundaries}, @var{labels}, @var{num_labels}] = } bwboundaries(@dots{})
 ## Trace the boundaries of the objects in a binary image.
 ##
 ## @var{boundaries} is a cell array in which each element is the boundary of an
-## object in the binary image @var{BW}. The boundary of an object is represented
-## by a @var{K} by 2 matrix where each row contains the @var{(x, y)} coordinates
-## of a point on the boundary.
+## object in the binary image @var{BW}. The clockwise boundary of each object is 
+## computed by the @code{boundary} function.
 ##
-## By default the boundaries are computed using 4-connectivity. This can be 
-## changed to 8-connectivity by setting @var{connectivity} to 8. Sadly, this
-## feature is not yet implemented.
+## By default the boundaries are computed using 8-connectivity. This can be 
+## changed to 4-connectivity by setting @var{conn} to 4.
 ##
 ## By default @code{bwboundaries} computes all boundaries in the image, i.e.
 ## both interior and exterior object boundaries. This behaviour can be changed
-## through the @var{options} input argument. If this is the string @t{"holes"}
-## both boundary types are considered. If it is instead @t{"noholes"}, only exterior
+## through the @var{holes} input argument. If this is @t{'holes'},
+## both boundary types are considered. If it is instead @t{'noholes'}, only exterior
 ## boundaries will be traced.
 ##
-## If two or more output arguments are requested, the algorithm also computes
-## the labelled image as returned by @code{bwlabel} in @var{labels}. The number
+## If two or more output arguments are requested, the algorithm also returns
+## the labelled image computed by @code{bwlabel} in @var{labels}. The number
 ## of labels in this image is optionally returned in @var{num_labels}.
-## @seealso{bwlabel}
+## @seealso{boundary, bwlabel}
 ## @end deftypefn
 
-function [B, L, num_labels] = bwboundaries (bw, N = 4, options = "holes")
-  ## Check input
+function [bound, labels, num_labels] = bwboundaries (bw, conn=8, holes="holes")
+  # check arguments
   if (nargin < 1)
     error ("bwboundaries: not enough input arguments");
   endif
   if (!ismatrix (bw) || ndims (bw) != 2)
     error ("bwboundaries: first input argument must be a NxM matrix");
   endif
-  if (!isscalar (N) || !any (N == [4])) #, 8]))
-    error ("bwboundaries: second input argument must be 4");
+  if (!isscalar (conn) || (conn != 4 && conn != 8))
+    error ("bwboundaries: second input argument must be 4 or 8");
   endif
-  if (!ischar (options) || !any (strcmpi (options, {"holes", "noholes"})))
-    error ("bwboundaries: third input must be either \"holes\" or \"noholes\"");
-  endif
-  
-  
-  ## Warn if the user request more output arguments than our implementation supports
-  if (nargout > 3)
-%    warning ("%s %s %s", ...
-%             "bwboundaries: adjacency matrix output is currently not supported. " ...
-%             "Please contact the Octave-Forge community if you want to contribute " ...
-%             "an implementation of this");
+  if (!ischar (holes) || !any (strcmpi (holes, {'holes', 'noholes'})))
+    error ("bwboundaries: third input must be either \'holes\' or \'noholes\'");
   endif
   
-  ## Make sure 'bw' is logical
   bw = logical (bw);
   
-  ## Found connected components in 'bw', and treat each of them seperatly
-  [L, num_labels] = bwlabel (bw, N);
-  B = cell (num_labels, 1);
-  for n = 1:num_labels
-    segment = (L == n);
-    [R, C] = find (segment);
-    if (numel (R) > 1)
-      ## XXX: support 8-neighbors
-%      B {n} = __imboundary__ (segment, 4, R (1), C (1));
-      B {n} = __imboundary__ (segment.', N, C (1), R (1));
-    else
-      B {n} = [R, C];
-    endif
+  # process each connected region separately
+  [labels, num_labels] = bwlabel (bw, conn);
+  bound = cell (num_labels, 1);
+  for k = 1:num_labels
+    bound {k} = __boundary__ ((labels == k), conn);
   endfor
   
-  ## If requested, compute internal boundaries as well
-  if (strcmpi (options, "holes"))
-    filled = bwfill (bw, "holes", N);
+  # compute internal boundaries as well?
+  if (strcmpi (holes, "holes"))
+    filled = bwfill (bw, "holes", conn);
     holes = (filled & !bw);
-    [internal, in_label, lin] = bwboundaries (holes, N, "noholes");
-    B (end+1:end+lin, 1) = internal;
-    
-    in_label (in_label != 0) += num_labels;
-    L += in_label;
+    [intBounds, intLabels, numIntLabels] = bwboundaries (holes, conn, "noholes");
+
+    bound (end+1 : end+numIntLabels, 1) = intBounds;
+    intLabels (intLabels != 0) += num_labels;
+    labels += intLabels;
   endif
 endfunction
+
+%!demo
+%! ## Generate a simple image
+%! bw = false (100);
+%! bw (10:30, 40:80) = true;
+%! bw (40:45, 40:80) = true;
+%!
+%! ## Find boundaries
+%! bounds = bwboundaries (bw);
+%!
+%! ## Plot result
+%! imshow (bw);
+%! hold on
+%! for k = 1:numel (bounds)
+%!   plot (bounds {k} (:, 2), bounds {k} (:, 1), 'r', 'linewidth', 2);
+%! endfor
+%! hold off
