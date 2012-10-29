efd3af142d067e678fb8a964d29a2a5f78c6b90e
carandraug <carandraug@localhost>
2012-10-29 03:23:26 +0000

    immaximas: fix license by using proper MIT license text after private conversation with copyright owner Peter Kovesi

--- inst/immaximas.m
+++ inst/immaximas.m
@@ -2,15 +2,24 @@
 ## School of Computer Science & Software Engineering
 ## The University of Western Australia
 ## http://www.csse.uwa.edu.au/
-##   
+##
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
-## in the Software without restriction, subject to the following conditions:
-## 
-## The above copyright notice and this permission notice shall be included in all
-## copies or substantial portions of the Software.
-## 
-## The Software is provided "as is", without warranty of any kind.
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+##
+## The software is provided "as is", without warranty of any kind, express or
+## implied, including but not limited to the warranties of merchantability,
+## fitness for a particular purpose and noninfringement. In no event shall the
+## authors or copyright holders be liable for any claim, damages or other
+## liability, whether in an action of contract, tort or otherwise, arising from,
+## out of or in connection with the software or the use or other dealings in the
+## software.
 ##
 ## I've made minor changes compared to the original 'nonmaxsuppts' function developed
 ## by Peter Kovesi. The original is available at
@@ -105,4 +114,3 @@ function varargout = immaximas(im, radius, thresh)
     varargout{nd+1} = im(ind);
   endif
 endfunction
-
