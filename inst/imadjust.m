35e8bab52657e36c848502ca1579bace288afd66
Carn? Draug <carandraug@octave.org>
2015-11-09 19:14:10 +0000

    imadjust: refactor for simplication, ND support, and coding guidelines.
    * inst/imadjust.m: input check has been rewritten to support ND
      images, IN, OUT, and GAMMA arguments in agreement with stretchlim()
      (since 3e053c93d141).  The actual calling of imadjust_plane() has
      been simplified with a single loop rather than branching conditions.
      Recode to follow Octave coding guidelines, include tests.  Add tests
      for N dimensional input.
    * NEWS: note support for ND support.

--- inst/imadjust.m
+++ inst/imadjust.m
@@ -1,27 +1,28 @@
 ## Copyright (C) 1999,2000 Kai Habel <kai.habel@gmx.de>
-## Copyright (C) 2004 Josep Mones i Teixidor <jmones@puntbarra.com>
+## Copyright (C) 2004 Josep Monés i Teixidor <jmones@puntbarra.com>
+## Copyright (C) 2015 Carnë Draug <carandraug@octave.org>
 ##
-## This program is free software; you can redistribute it and/or modify it under
-## the terms of the GNU General Public License as published by the Free Software
-## Foundation; either version 3 of the License, or (at your option) any later
-## version.
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
 ##
-## This program is distributed in the hope that it will be useful, but WITHOUT
-## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-## details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License along with
-## this program; if not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{J} =} imadjust (@var{I})
-## @deftypefnx {Function File} {@var{J} =} imadjust (@var{I},[@var{low_in};@var{high_in}])
-## @deftypefnx {Function File} {@var{J} =} imadjust (@var{I},[@var{low_in};@var{high_in}],[@var{low_out};@var{high_out}])
-## @deftypefnx {Function File} {@var{J} =} imadjust (@dots{}, @var{gamma})
-## @deftypefnx {Function File} {@var{newmap} =} imadjust (@var{map}, @dots{})
-## @deftypefnx {Function File} {@var{RGB_out} =} imadjust (@var{RGB}, @dots{})
-## Adjust image or colormap values to a specified range.
+## @deftypefn  {Function File} {} imadjust (@var{I})
+## @deftypefnx {Function File} {} imadjust (@var{I}, [@var{low_in}; @var{high_in}])
+## @deftypefnx {Function File} {} imadjust (@var{I}, [@var{low_in}; @var{high_in}],[@var{low_out}; @var{high_out}])
+## @deftypefnx {Function File} {} imadjust (@dots{}, @var{gamma})
+## @deftypefnx {Function File} {} imadjust (@var{cmap}, @dots{})
+## @deftypefnx {Function File} {} imadjust (@var{RGB}, @dots{})
+## Adjust image or colormap intensity (values).
 ##
 ## @code{J=imadjust(I)} adjusts intensity image @var{I} values so that
 ## 1% of data on lower and higher values (2% in total) of the image is
@@ -91,67 +92,66 @@
 ## @seealso{stretchlim, brighten}
 ## @end deftypefn
 
-## TODO: When Octave 2.1.58 is out multiply indices if input argument is
-## TODO: of class int* or uint*.
+function img = imadjust (img, in, out = [0; 1], gamma = 1)
 
-function ret = imadjust (image, in = stretchlim (image), out = [0;1], gamma = 1)
-
-  if (nargin < 1 || nargin > 4)
-    print_usage;
+  if (nargin () < 1 || nargin () > 4)
+    print_usage ();
   endif
 
-  if (! isimage (image) && ! iscolormap (image))
-    error ("imadjust(image,...) first parameter must be a image matrix or colormap");
+  if (! isimage (img))
+    error ("imadjust: I, RGB, or CMAP must be an image or a colormap");
   endif
 
-  ## IN and OUT can be empty to use default values
-  if (! isnumeric (in) || ! isnumeric (out))
-    error ("imadjust: IN and OUT must be numeric arrays");
-  endif
-
-  if (isempty (in))
-    in=[0;1];               ## default in
-  endif
-
-  if (isempty (out))
-    out=[0;1];               ## default out
-  endif
-
-  simage=size(image);
-  if (length(simage)==3 && simage(3)==3)
-    ## image is rgb
-    [in, out, gamma]=__imadjust_check_3d_args__(in, out, gamma);
-
-    ## make room
-    ret=zeros(size(image));
-
-    ## process each plane
-    for i=1:3
-      ret(:,:,i)=__imadjust_plane__(image(:,:,i),in(1,i),in(2,i),out(1,i),out(2,i),gamma(1,i));
-    endfor
-
-  elseif (length(simage)==2)
-    if(simage(2)==3 && ...
-       (size(in)==[2,3] || size(out)==[2,3] || size(gamma)==[1,3]) )
-      ## image is a colormap
-      [in, out, gamma]=__imadjust_check_3d_args__(in, out, gamma);
-
-      ret=[];
-      ## process each color
-      for i=1:3
-        ret=horzcat(ret,__imadjust_plane__(image(:,i),in(1,i),in(2,i),out(1,i),out(2,i),gamma(i)));
-      endfor
-
-    else
-      ## image is a intensity image
-      if( !isvector(in) || length(in)!=2 || !isvector(out) || length(out)!=2 || !isscalar(gamma) || (gamma<0) || (gamma==Inf) )
-        error("imadjust: on an intensity image, in and out must be 2-by-1 and gamma a positive scalar.");
-      endif
-      ret=__imadjust_plane__(image,in(1),in(2),out(1),out(2),gamma);
-    endif
-
+  sz = size (img);
+  if (numel (sz) == 2 && sz(2) == 3 && isa (img, "double"))
+    was_colormap = true;
+    img = reshape (img, [sz(1) 1 sz(2)]);
+    sz = size (img);
   else
-    error("imadjust: first parameter must be a colormap, an intensity image or a RGB image");
+    was_colormap = false;
+  endif
+  n_planes = prod (sz(3:end));
+
+
+  if (nargin () < 2)
+    in = stretchlim (img, 0.01);
+  else
+    in = parse_limits (in, sz);
+  endif
+  out = parse_limits (out, sz);
+
+  if (! isfloat (gamma) || any (gamma < 0))
+    error ("imadjust: GAMMA must be a non-negative floating point")
+  elseif (isscalar (gamma))
+    gamma = repmat (gamma, [n_planes, 1]);
+  elseif (! isequal (size (gamma)(2:end), sz(3:end)))
+    error ("imadjust: GAMMA must be a scalar or 1 row per plane")
+  endif
+
+  for i = 1:n_planes
+    img(:,:,i) = imadjust_plane (img(:,:,i), in(:,i), out(:,i), gamma(i));
+  endfor
+
+  if (was_colormap)
+    img = reshape (img, [sz(1) sz(3)]);
+  endif
+
+endfunction
+
+function limits = parse_limits (limits, sz)
+  if (isempty (limits))
+    limits = repmat ([0; 1], [1 sz(3:end)]);
+  else
+    if (! isfloat (limits))
+      error ("imadjust: IN and OUT must be numeric floating-point arrays");
+    endif
+    ## Only reshape back into 2 row column for a single plane.
+    ## Require the correct format otherwise.
+    if (numel (limits) == 2)
+      limits = repmat (limits(:), [1 sz(3:end)]);
+    elseif (rows (limits) != 2 || ! isequal (sz(3:end), size (limits)(2:end)))
+      error ("imadjust: IN and OUT must be a 2 row column per plane");
+    endif
   endif
 endfunction
 
@@ -159,162 +159,156 @@ endfunction
 ## values; and lo and ho, output bottom and top values.
 ## Image negative is computed if ho<lo although nothing special is
 ## needed, since formula automatically handles it.
-function ret=__imadjust_plane__(I, li, hi, lo, ho, gamma)
+function ret = imadjust_plane (I, in, out, gamma)
+  li = in(1);
+  hi = in(2);
+  lo = out(1);
+  ho = out(2);
   ret = (I < li) .* lo;
   ret = ret + (I >= li & I < hi) .* (lo + (ho - lo) .* ((I - li) / (hi - li)) .^ gamma);
   ret = ret + (I >= hi) .* ho;
 endfunction
 
-## Checks in, out and gamma to see if they are ok for colormap and RGB
-## cases.
-function [in, out, gamma]=__imadjust_check_3d_args__(in, out, gamma)
-  switch(size(in)) 
-    case([2,3]) 
-      ## ok!
-    case([2,1])
-      in=repmat(in,1,3);
-    case([1,2]) ## Compatibility behaviour!
-      in=repmat(in',1,3);
-    otherwise
-      error("imadjust: in must be 2-by-3 or 2-by-1.");
-  endswitch
-  
-  switch(size(out))
-    case([2,3])
-      ## ok!
-    case([2,1])
-      out=repmat(out,1,3);
-    case([1,2]) ## Compatibility behaviour!
-      out=repmat(out',1,3);
-    otherwise
-      error("imadjust: out must be 2-by-3 or 2-by-1.");
-  endswitch
-  
-  switch(size(gamma))
-    case([1,3])
-      ## ok!
-    case([1,1])
-      gamma=repmat(gamma,1,3);
-    otherwise
-      error("imadjust: gamma must be a scalar or a 1-by-3 matrix.");
-  endswitch
-  
-  ## check gamma allowed range
-  if(!all((gamma>=0)&(gamma<Inf)))
-    error("imadjust: gamma values must be in the range [0,Inf]");
-  endif
 
-endfunction
+%!error <must be an image or a colormap> imadjust ("bad argument");
+%!error <numeric floating-point arrays> imadjust ([1:100], "bad argument", [], 1);
+%!error <2 row column per plane> imadjust ([1:100], [0 1 1], [], 1);
+%!error <2 row column per plane> imadjust ([1:100], [], [0 1 1], 1);
+%!error <scalar or 1 row per plane> imadjust ([1:100], [], [], [0; 1]);
+%!error <scalar or 1 row per plane> imadjust (rand (5, 5, 3), [], [], [0 1]);
+%!error <non-negative floating point> imadjust ([1:100], [0; 1], [], -1);
 
-# bad arguments
 
-# bad images
-%!error(imadjust("bad argument"));
-%!error(imadjust(zeros(10,10,3,2)));
-%!error(imadjust(zeros(10,10,4)));
-%!error(imadjust("bad argument"));
+## Test default values to 1% on each end saturated and [] as [0; 1]
+%!test
+%! im = [0.01:0.01:1];
+%! assert (imadjust (im), [0 linspace(0, 1, 98) 1], eps)
+%! assert (imadjust (im), imadjust (im, stretchlim (im, 0.01), [0; 1], 1))
+%! assert (imadjust (im, []), imadjust (im, [0; 1], [0; 1], 1))
+%! assert (imadjust (im, [], []), imadjust (im, [0; 1], [0; 1], 1))
+%! assert (imadjust (im, [], [.25 .75]), imadjust (im, [0; 1], [.25; .75], 1))
+%! assert (imadjust (im, [.25; .75], []), imadjust (im, [.25; .75], [0; 1], 1))
 
-# bad 2d, 3d or 4th argument
-%!error(imadjust([1:100],"bad argument",[0;1],1));
-%!error(imadjust([1:100],[0,1,1],[0;1],1));
-%!error(imadjust([1:100],[0;1],[0,1,1],1));
-%!error(imadjust([1:100],[0;1],[0;1],[0;1]));
-%!error(imadjust([1:100],[0;1],[0;1],-1));
+%!assert (imadjust (linspace (0, 1), [], [.25 .75]), linspace (.25, .75, 100))
 
-%!# 1% on each end saturated
-%!assert(imadjust([1:100]),[0,linspace(0,1,98),1]);
+## test with only input arg
+%!assert (sum (abs ((imadjust (linspace (0, 1, 100),[1/99; 98/99])
+%!                   - [0 linspace(0, 1, 98) 1])(:))) < 1e-10)
 
-%!# test with only input arg
-%!assert(sum(abs((imadjust(linspace(0,1,100),[1/99;98/99]) -
-%!                [0,linspace(0,1,98),1] )(:))) < 1e-10);
+## a test with input and output args
+%!assert (imadjust ([1:100], [50; 90],[-50; -30]),
+%!       [-50*ones(1,49), linspace(-50,-30,90-50+1), -30*ones(1,10)])
 
-%!# a test with input and output args
-%!assert(imadjust([1:100],[50;90],[-50;-30]),
-%!       [-50*ones(1,49), linspace(-50,-30,90-50+1), -30*ones(1,10)]);
+## a test with input and output args in a row vector (Compatibility behaviour)
+%!assert (imadjust ([1:100], [50; 90],[-50; -30]),
+%!        [repmat(-50, [1 49]) linspace(-50, -30, 90-50+1) repmat(-30, [1 10])])
 
-%!# a test with input and output args in a row vector (Compatibility behaviour)
-%!assert(imadjust([1:100],[50,90],[-50,-30]),
-%!       [-50*ones(1,49), linspace(-50,-30,90-50+1), -30*ones(1,10)]);
-
-%!# the previous test, "negated"
-%!assert(imadjust([1:100],[50;90],[-30;-50]),
-%!       [-30*ones(1,49), linspace(-30,-50,90-50+1), -50*ones(1,10)]);
+## the previous test, "negated"
+%!assert (imadjust ([1:100], [50; 90],[-30; -50]),
+%!        [repmat(-30, [1 49]) linspace(-30, -50, 90-50+1) repmat(-50, [1 10])])
 
 %!shared cm,cmn
-%! cm=[[1:10]',[2:11]',[3:12]'];
-%! cmn=([[1:10]',[2:11]',[3:12]']-1)/11;
+%! cm = [[1:10]' [2:11]' [3:12]'];
+%! cmn = ([[1:10]' [2:11]' [3:12]'] -1)/11;
 
-%!# a colormap
-%!assert(imadjust(cmn,[0;1],[10;11]),cmn+10);
+## a colormap
+%!assert (imadjust (cmn, [0; 1], [10; 11]), cmn+10)
 
-%!# a colormap with params in row (Compatibility behaviour)
-%!assert(imadjust(cmn,[0,1],[10,11]),cmn+10);
+## a colormap with params in row (Compatibility behaviour)
+%!assert (imadjust (cmn, [0 1], [10 11]), cmn+10)
 
-%!# a colormap, different output on each
-%!assert(imadjust(cmn,[0;1],[10,20,30;11,21,31]),cmn+repmat([10,20,30],10,1));
+## a colormap, different output on each
+%!assert (imadjust (cmn, [0; 1], [10 20 30; 11 21 31]),
+%!        cmn + repmat ([10 20 30], 10, 1))
 
-%!# a colormap, different input on each, we need increased tolerance for this test
-%!assert(sum(abs((imadjust(cm,[2,4,6;7,9,11],[0;1]) -
+## a colormap, different input on each
+%!assert (imadjust (cm, [2 4 6; 7 9 11], [0; 1]),
 %!       [[0 linspace(0, 1, 6) 1 1 1]' ...
 %!        [0 0 linspace(0, 1, 6) 1 1]' ...
-%!        [0 0 0 linspace(0, 1, 6) 1]']
-%!       ))(:)) < 1e-10
-%!       );
+%!        [0 0 0 linspace(0, 1, 6) 1]'], eps)
 
-%!# a colormap, different input and output on each
-%!assert(sum(abs((imadjust(cm,[2,4,6;7,9,11],[0,1,2;1,2,3]) -
-%!       [[0 linspace(0, 1, 6) 1 1 1]' ...
-%!        [0 0 linspace(0, 1, 6) 1 1]'+1, ...
-%!        [0 0 0 linspace(0, 1, 6) 1]'+2]
-%!       ))(:)) < 1e-10
-%!       );
+## a colormap, different input and output on each
+%!assert (imadjust (cm, [2 4 6; 7 9 11], [0 1 2; 1 2 3]),
+%!        [[0 linspace(0, 1, 6) 1 1 1]' ...
+%!         [0 0 linspace(0, 1, 6) 1 1]'+1 ...
+%!         [0 0 0 linspace(0, 1, 6) 1]'+2], eps)
 
-%!# a colormap, different gamma, input and output on each
-%!assert(sum(abs((imadjust(cm,[2,4,6;7,9,11],[0,1,2;1,2,3],[1,2,3]) -
-%!       [[0 linspace(0, 1, 6) 1 1 1]', ...
-%!        [0 0 linspace(0, 1, 6).^2 1 1]'+1, ...
-%!        [0 0 0 linspace(0, 1, 6).^3 1]'+2]
-%!       )(:))) < 1e-10
-%!       );
+## a colormap, different gamma, input and output on each
+%!assert (imadjust (cm, [2 4 6; 7 9 11], [0 1 2; 1 2 3], [1 2 3]),
+%!        [[0 linspace(0, 1, 6) 1 1 1]' ...
+%!         [0 0 linspace(0, 1, 6).^2 1 1]'+1 ...
+%!         [0 0 0 linspace(0, 1, 6).^3 1]'+2], eps)
 
 %!shared iRGB,iRGBn,oRGB
-%! iRGB=zeros(10,1,3);
-%! iRGB(:,:,1)=[1:10]';
-%! iRGB(:,:,2)=[2:11]';
-%! iRGB(:,:,3)=[3:12]';
-%! iRGBn=(iRGB-1)/11;
-%! oRGB=zeros(10,1,3);
-%! oRGB(:,:,1)=[0,linspace(0,1,6),1,1,1]';
-%! oRGB(:,:,2)=[0,0,linspace(0,1,6),1,1]';
-%! oRGB(:,:,3)=[0,0,0,linspace(0,1,6),1]';
+%! iRGB = zeros (10 ,1, 3);
+%! iRGB(:,:,1) = [1:10]';
+%! iRGB(:,:,2) = [2:11]';
+%! iRGB(:,:,3) = [3:12]';
+%! iRGBn = (iRGB-1) /11;
+%! oRGB = zeros (10, 1, 3);
+%! oRGB(:,:,1) = [0 linspace(0,1,6) 1 1 1]';
+%! oRGB(:,:,2) = [0 0 linspace(0,1,6) 1 1]';
+%! oRGB(:,:,3) = [0 0 0 linspace(0,1,6) 1]';
 
-%!# a RGB image
-%!assert(imadjust(iRGBn,[0;1],[10;11]),iRGBn+10);
+## a RGB image
+%!assert (imadjust (iRGBn, [0; 1], [10; 11]), iRGBn+10)
 
-%!# a RGB image, params in row (compatibility behaviour)
-%!assert(imadjust(iRGBn,[0,1],[10,11]),iRGBn+10);
+## a RGB image, params in row (compatibility behaviour)
+%!assert (imadjust (iRGBn, [0 1], [10 11]), iRGBn+10)
 
-%!# a RGB, different output on each
+## a RGB, different output on each
 %!test
-%! t=iRGBn;
-%! t(:,:,1)+=10;
-%! t(:,:,2)+=20;
-%! t(:,:,3)+=30;
-%! assert(imadjust(iRGBn,[0;1],[10,20,30;11,21,31]),t);
+%! t = iRGBn;
+%! t(:,:,1) += 10;
+%! t(:,:,2) += 20;
+%! t(:,:,3) += 30;
+%! assert (imadjust (iRGBn, [0; 1], [10 20 30; 11 21 31]), t)
 
-%!# a RGB, different input on each, we need increased tolerance for this test
-%!assert(sum(abs((imadjust(iRGB,[2,4,6;7,9,11],[0;1]) - oRGB)(:))) < 1e-10);
+## a RGB, different input on each, we need increased tolerance for this test
+%!assert (imadjust (iRGB, [2 4 6; 7 9 11], [0; 1]), oRGB, eps)
 
-%!# a RGB, different input and output on each
+## a RGB, different input and output on each
 %!test
-%! t=oRGB;
-%! t(:,:,2)+=1;
-%! t(:,:,3)+=2;
-%! assert(sum(abs((imadjust(iRGB,[2,4,6;7,9,11],[0,1,2;1,2,3]) - t)(:))) < 1e-10);
+%! t = oRGB;
+%! t(:,:,2) += 1;
+%! t(:,:,3) += 2;
+%! assert (imadjust (iRGB, [2 4 6; 7 9 11], [0 1 2;1 2 3]), t, eps)
 
-%!# a RGB, different gamma, input and output on each
+## a RGB, different gamma, input and output on each
 %!test
-%! t=oRGB;
-%! t(:,:,2)=t(:,:,2).^2+1;
-%! t(:,:,3)=t(:,:,3).^3+2;
-%! assert(sum(abs((imadjust(iRGB,[2,4,6;7,9,11],[0,1,2;1,2,3],[1,2,3]) - t)(:))) < 1e-10);
+%! t = oRGB;
+%! t(:,:,2) = t(:,:,2).^2+1;
+%! t(:,:,3) = t(:,:,3).^3+2;
+%! assert (imadjust (iRGB, [2 4 6; 7 9 11], [0 1 2; 1 2 3], [1 2 3]), t, eps)
+
+## Test for ND dimensional images
+%!test
+%! img = rand (4, 4, 2, 3, 4);
+%! adj = zeros (4, 4, 2, 3, 4);
+%! for p = 1:2
+%!   for q = 1:3
+%!     for r = 1:4
+%!       adj(:,:,p,q,r) = imadjust (img(:,:,p,q,r));
+%!     endfor
+%!   endfor
+%! endfor
+%! assert (imadjust (img), adj)
+
+## Test for ND dimensional images with N dimensional arguments
+%!test
+%! img = rand (4, 4, 2, 3, 2);
+%! adj = zeros (4, 4, 2, 3, 2);
+%! in  = reshape ([ 3  5  7  9 11 13 15 17 19 21 23 25;
+%!                 97 95 93 91 89 87 85 83 81 79 77 75] / 100, [2 2 3 2]);
+%! out = reshape ([ 5  7  9 11 14 15 17 19 21 23 25 27;
+%!                 95 93 91 89 87 85 83 81 79 77 75 73] / 100, [2 2 3 2]);
+%! gamma = reshape (0.6:.1:1.7, [1 2 3 2]);
+%! for p = 1:2
+%!   for q = 1:3
+%!     for r = 1:2
+%!       adj(:,:,p,q,r) = imadjust (img(:,:,p,q,r), in(:,p,q,r),
+%!                                  out(:,p,q,r), gamma(1,p,q,r));
+%!     endfor
+%!   endfor
+%! endfor
+%! assert (imadjust (img, in, out, gamma), adj)
