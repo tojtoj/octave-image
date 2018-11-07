cace6a765197fbea384c1b0198e2df2502dd65d9
Carn? Draug <carandraug@octave.org>
2018-11-07 13:36:27 +0000

    bwunpack.m: multiple style changes, mainly on input checking (patch #9174)
    * inst/bwunpack.m: do not keep checking if m exists, check nargin at
      the start, and then define a default m; don't use 32 the sizeof
      uint32, use variable for that; add more tests for different
      dimensions of length zero; add check for M being larger than the
      number of bits available; check error message instead of general
      error ids to ensure the error is what we really expect;  don't
      name output variable on documentation because there's only one;
      list both function signatures in documentation (with and without
      M defined).

--- inst/bwunpack.m
+++ inst/bwunpack.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2018 Martin Janda <janda.martin1@gmail.com>
+## Copyright (C) 2018 David Miguel Susano Pinto <carandraug@octave.org>
 ##
 ## This program is free software: you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
@@ -15,55 +16,74 @@
 ## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {} {@var{BW} =} bwunpack (@var{BWP}, @var{m})
+## @deftypefn  {} {} bwunpack (@var{bwp})
+## @deftypefnx {} {} bwunpack (@var{bwp}, @var{m})
 ## Unpack binary image.
 ##
-## Each row of a packed binary image @var{BWP} represented as a uint32 matrix
-## is unpacked into 32 rows of pixels of the original image @var{BW} such that
-## the least significant bit of each uint32 element maps to the pixel in the
-## first @var{BW} row and the most significant bit to pixel of the 32th row.
+## Each row of the packed binary image @var{bwp}, represented as an
+## uint32 matrix, is unpacked into 32 rows of logical values.  The
+## unpacking is done such that the least significant bit of the first
+## element in @var{bwp} maps to the first element in the unpacked
+## image, and the most significant bit to the 32th element.
 ##
-## @var{BW} is @var{m}xN logical matrix with the same number of dimensions as
-## @var{BWP} (possibly more than two).  If @var{m} is not specified, it
-## defaults to @code{size(BWP, 1) * 32}.  The number of output rows is always
-## @code{min(m, size(BWP, 1) * 32)}.
-##
-## Implementation note: uses bitunpack internally.
+## The unpacked image will be a logical array with @var{m} rows.
+## The length of the other dimensions will be the same as @var{bwp}.
+## If @var{m} is not specified, it will unpack all bits in @var{bwp},
+## otherwise the extra bits will be considered padding resulting
+## from the packing.  See the help text for @code{bwpack} for
+## details.
 ##
 ## @seealso{bwpack, bitpack, bitunpack}
 ## @end deftypefn
 
-function BW = bwunpack (BWP, m)
+function bw = bwunpack (bwp, m)
   if (nargin < 1)
     print_usage ();
   endif
 
-  if (! isa (BWP, "uint32"))
-    error("Octave:invalid-input-arg", "BWP must be an uint32 matrix");
+  if (! isa (bwp, "uint32"))
+    error ("Octave:invalid-input-arg", "bwunpack: BWP must be an uint32 array");
   endif
 
-  if (exist ("m") && ! (floor(m) == m && m >= 0))
-    error("Octave:invalid-input-arg", "m must be a non-negative integer");
+  class_size = 32; # number of elements packed into a uint32
+
+  if (nargin < 2)
+    m = rows (bwp) * class_size;
+  elseif (m < 0 || fix (m) != m)
+    error ("Octave:invalid-input-arg",
+           "bwunpack: M must be a non-negative integer");
+  elseif (m > (rows (bwp) * class_size))
+    error ("Octave:invalid-input-arg",
+           ["bwunpack: M must not be larger than the number of bits " ...
+              "on each column"]);
   endif
 
-  dim = size (BWP);
-  dim(1) = dim(1) * 32;
-  BW = reshape (bitunpack (BWP), dim);
-
-  if (! exist ("m"))
-    m = size (BWP, 1) * 32;
-  endif
-  BW(m+1:end, :) = [];
+  packed_rows = rows (bwp) * class_size;
+  dims = size (bwp);
+  bw = reshape (bitunpack (bwp), [packed_rows dims(2:end)]);
+  bw(m+1:end,:) = []; # remove padding if any
 endfunction
 
-## test argument checking
-%!error id=Octave:invalid-fun-call bwunpack()
-%!error id=Octave:invalid-input-arg bwunpack(uint8(ones(3, 3)))
-%!error id=Octave:invalid-input-arg bwunpack(uint32(ones(3, 3)), -1)
-%!error id=Octave:invalid-input-arg bwunpack(uint32(ones(3, 3)), 4.2)
+%!error id=Octave:invalid-fun-call bwunpack ()
+%!error <BWP must be an uint32 array> bwunpack (uint8 (1))
+%!error <M must be a non-negative integer> bwunpack (uint32 (1), -1)
+%!error <M must be a non-negative integer> bwunpack (uint32 (1), 4.2)
 
-## simple cases
-%!assert (bwunpack(uint32([])), logical([]))
-%!assert (bwunpack(uint32([]), 0), logical([]))
-%!assert (bwunpack(uint32(2.^[0:31])), logical(eye(32)))
-%!assert (bwunpack(uint32(7 * ones(1, 3, 3, 3)), 3), logical(ones(3, 3, 3, 3)))
+%!assert (bwunpack (uint32 (2.^[0:31])), logical (eye (32)))
+
+%!assert (bwunpack (uint32 (repmat (7, [1 3 3 3])), 3), true (3, 3, 3, 3))
+
+%!assert (bwunpack (uint32 (zeros (0, 0))), false (0, 0))
+%!assert (bwunpack (uint32 (zeros (0, 0)), 0), false (0, 0))
+%!assert (bwunpack (uint32 (zeros (0, 5)), 0), false (0, 5))
+%!assert (bwunpack (uint32 (zeros (0, 5, 7)), 0), false (0, 5, 7))
+%!assert (bwunpack (uint32 (zeros (1, 0))), false (32, 0))
+%!assert (bwunpack (uint32 (zeros (2, 0, 7))), false (64, 0, 7))
+%!assert (bwunpack (uint32 (zeros (2, 0, 7))), false (64, 0, 7))
+%!assert (bwunpack (uint32 (zeros (2, 0, 7)), 60), false (60, 0, 7))
+
+## Unpacking more bytes than what's available on the input needs to be
+## an error.  This works in Matlab but it's their bug and the result
+## is not even reproducible.
+%!error <M must not be larger than the number of bits on each column>
+%!      bwunpack (uint32 (1), 1042)
