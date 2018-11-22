a473620955bdf9469691cecc1d76d6f9f41e2c3d
Martin Janda <janda.martin1@gmail.com>
2018-11-22 21:59:19 +0000

    colorangle.m: use rad2deg and return 0 instead of NaN (patch #9608)

--- inst/colorangle.m
+++ inst/colorangle.m
@@ -41,21 +41,39 @@
 ## Created: 2018-03-26
 
 function [angle] = colorangle (rgb1, rgb2)
-  if (nargin != 2 || length (rgb1) != 3 || length (rgb2) != 3)
+  if (nargin != 2)
     print_usage ();
   endif
+
+  validateattributes (rgb1, {"numeric"}, {"real", "vector", "numel", 3},
+                      "colorangle", "rgb1");
+  validateattributes (rgb2, {"numeric"}, {"real", "vector", "numel", 3},
+                      "colorangle", "rgb2");
+
   norm1 = norm (rgb1);
   norm2 = norm (rgb2);
   if (norm1 != 0 && norm2 != 0)
-    angle = (180/pi) * acos (dot (rgb1, rgb2) / (norm1 * norm2));
-    # [TODO] in the future substitute (180/pi) by rad2deg ()
+    angle = rad2deg (acos ( dot (rgb1, rgb2) / (norm1 * norm2)));
   else
-    angle = NaN;
+    angle = 0; # could be NaN but MATLAB outputs 0
   endif
 endfunction
 
-%!test
-%! assert(colorangle ([1 1 1], [1 1 1]), 0, 0.0001)
-%! assert(colorangle ([1 0 0], [-1 0 0]), 180, 0.0001)
-%! assert(colorangle ([0 0 1], [1 0 0]), 90, 0.0001)
-%! assert(colorangle ([0.5 0.61237 -0.61237], [0.86603 0.35355 -0.35355]), 30, 0.0001)
+%!error id=Octave:invalid-fun-call colorangle ()
+%!error id=Octave:invalid-fun-call colorangle (1, 2, 3)
+%!error id=Octave:incorrect-numel colorangle (2, 3)
+%!error id=Octave:incorrect-numel colorangle ([1, 2], [3, 4])
+%!error id=Octave:expected-real colorangle ([1, 2, 3j], [4, 5, 6])
+%!error id=Octave:expected-real colorangle ([1, 2, 3], [4j, 5, 6])
+%!error id=Octave:invalid-type colorangle ("abc", "def")
+
+%!assert (colorangle ([0 0 0], [0 0 0]), 0, 1e-4)
+%!assert (colorangle ([1 1 1], [1 1 1]), 0, 1e-4)
+%!assert (colorangle ([1 0 0], [-1 0 0]), 180, 1e-4)
+%!assert (colorangle ([0 0 1], [1 0 0]), 90, 1e-4)
+%!assert (colorangle ([0; 0; 1], [1 0 0]), 90, 1e-4)
+%!assert (colorangle ([0, 0, 1], [1; 0; 0]), 90, 1e-4)
+%!assert (colorangle ([0.5 0.61237 -0.61237], [0.86603 0.35355 -0.35355]), 30.000270917, 1e-4)
+%!assert (colorangle ([0.1582055390, 0.2722362096, 0.1620813305], [0.0717 0.1472 0.0975]), 5.09209927, 1e-6)
+%!assert (colorangle ([0.0659838500, 0.1261619536, 0.0690643667], [0.0717 0.1472 0.0975]), 5.10358588, 1e-6)
+%!assert (colorangle ([0.436871170, 0.7794672250, 0.4489702582], [0.0717 0.1472 0.0975]), 5.01339769, 1e-6)
